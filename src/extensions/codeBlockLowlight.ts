93943cb0e0302c98bcea0ff9e3bdffc51950feb2
YanivWang <yanivwang@gmail.com>
2026-05-19 16:00:30 +0800

    feat: 修复 bug

--- src/extensions/codeBlockLowlight.ts
+++ src/extensions/codeBlockLowlight.ts
@@ -4,7 +4,6 @@
  */
 
 import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
-import xml from "highlight.js/lib/languages/xml";
 import { createLowlight, common } from "lowlight";
 
 import { DEFAULT_CODE_BLOCK_LANGUAGE } from "@/configs/editorConstants";
@@ -13,8 +12,8 @@ export { DEFAULT_CODE_BLOCK_LANGUAGE };
 
 const lowlight = createLowlight(common);
 
-// common 未包含 html，用 xml 语法注册 html 别名
-lowlight.register("html", xml);
+// common 含 xml 但无 html 键，注册为 html 别名供语言选择器使用
+lowlight.register("html", common.xml);
 
 export const codeBlockLowlightExtension = CodeBlockLowlight.configure({
   lowlight,
